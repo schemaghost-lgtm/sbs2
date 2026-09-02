bd86a30c2ad5b892594254b428d64fcb6aa1815e
schemaghost-lgtm <schemaghost@gmail.com>
2026-09-02 15:08:50 +0300

    Update MirrorView.kt

--- app/src/main/java/com/veil/sbsbrowser/MirrorView.kt
+++ app/src/main/java/com/veil/sbsbrowser/MirrorView.kt
@@ -1,7 +1,9 @@
 package com.veil.sbsbrowser
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
 
@@ -10,10 +12,26 @@ class MirrorView @JvmOverloads constructor(
     attrs: AttributeSet? = null
 ) : View(context, attrs) {
 
-    var source: MasterWebView? = null
+    var source: View? = null
+
+    var useCapturedFrame: Boolean = false
+    private var capturedFrame: Bitmap? = null
+    private val destRect = Rect()
+
+    fun setCapturedFrame(bitmap: Bitmap) {
+        capturedFrame = bitmap
+        postInvalidateOnAnimation()
+    }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        source?.draw(canvas)
+        if (useCapturedFrame) {
+            capturedFrame?.let { bmp ->
+                destRect.set(0, 0, width, height)
+                canvas.drawBitmap(bmp, null, destRect, null)
+            }
+        } else {
+            source?.draw(canvas)
+        }
     }
 }
